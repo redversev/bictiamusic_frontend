3d52ce8069e2d819428af76f7a22d019a662fca4
mayeballen <mayerliballen@gmail.com>
2020-03-21 17:23:06 -0500

    ModuloPrincipal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { PrincipalComponent } from "./components/principal/principal.component";
+import { NavLoginComponent } from "./components/nav-login/nav-login.component";
+import { FormLoginUserComponent } from "./components/form-login-user/form-login-user.component";
+import { FormRegisterUserComponent } from "./components/form-register-user/form-register-user.component";
+
+const register: Routes = [
+  { path: "registerUser", component: FormRegisterUserComponent },
+  { path: "", component: PrincipalComponent },
+  { path: "loginUser", component: FormLoginUserComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    NavLoginComponent,
+    FormLoginUserComponent,
+    FormRegisterUserComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(register)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/form-login-user/form-login-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-login-user',
+  templateUrl: './form-login-user.component.html',
+  styleUrls: ['./form-login-user.component.css']
+})
+export class FormLoginUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
