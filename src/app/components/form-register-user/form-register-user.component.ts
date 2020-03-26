68513c2e88ed2b37e3f26ddbf8fc268aa731f9e9
Redvers Escobar Vaquer <redversescobar@gmail.com>
2020-03-26 15:42:03 -0500

    Validación front del formulario de registro de usuario

--- src/app/components/form-register-user/form-register-user.component.ts
+++ src/app/components/form-register-user/form-register-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-form-register-user',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormRegisterUserComponent implements OnInit {
 
-  constructor() { }
+  public user: User;
+  public isError = false;
+  public messageError = "Los datos del formulario son incorrectos.";
+  public formLogin: Form;
+
+  constructor( ) { 
+    this.user = new User();
+  } 
 
   ngOnInit(): void {
   }
 
+  validateForm(formLogin) {
+    console.log(formLogin);
+    if(formLogin.valid== false){ 
+          this.isError = true;
+          setTimeout(() => {
+              this.isError = false;
+              this.messageError = "Los datos del formulario son incorrectos.";
+          }, 3000);
+    }
+    else{
+      console.log(formLogin);
+      if(formLogin.form.value.password != formLogin.form.value.passwordConfirm){
+          this.messageError= "Los contraseñas no coinciden";
+          this.isError = true;
+          setTimeout(() => {
+              this.isError = false;
+              this.messageError = "Los datos del formulario son incorrectos.";
+          }, 3000);
+      }
+      else{
+        alert("Los datos son correctos");
+        console.log("Aquí va la conexión con la API");
+      }
+
+    }
+  }
+
+  
 }
---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    _id: String;
+    firstName: String;
+    lastName: String;
+    phone: String;
+    email: String;
+    password: String;
+    passwordConfirm: String;
+    image: String;
+    terms: boolean;
+}
