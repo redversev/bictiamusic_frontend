1e24f7cbd480435bf019abef233bc0ee79b22d52
Redvers Escobar Vaquer <redversescobar@gmail.com>
2020-04-02 17:29:28 -0500

    correcciones

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/user";
+import { UserService } from "../../service/user.service";
+import { Router, RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  public user: User;
+  public role = null;
+
+
+  constructor() { 
+    this.user = new User();
+  }
+
+  ngOnInit(): void {
+    this.getRol();
+  }
+
+  getRol() {
+    this.user = JSON.parse(localStorage.getItem("user"));
+    this.role = this.user.role;
+  }
+
+}
