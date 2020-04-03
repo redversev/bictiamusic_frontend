da1d427b90be2fdeb5f5852cf6258faa9c02463f
CamiloLM <kmilo_16_16@hotmail.com>
2020-04-02 21:34:29 -0500

    Music controls funcionalidad y estilo, music list funcionalidad

--- src/app/components/music-list/music-list.component.ts
+++ src/app/components/music-list/music-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SongService } from "../../service/song.service";
 
 @Component({
   selector: 'app-music-list',
@@ -79,15 +80,18 @@ export class MusicListComponent implements OnInit {
     }
   ];
 
-  changeSong(urlSong) {
-    const video: HTMLMediaElement = document.getElementById('bictiaMusic') as HTMLMediaElement;
-    video.setAttribute('src', urlSong + '.mp3');
-    video.play();
-  }
-
-  constructor() { }
+  constructor(private service: SongService) { }
 
   ngOnInit(): void {
+  }
+
+  changeSong(song) {
+    const audio: HTMLMediaElement = document.getElementById('bictiaMusic') as HTMLMediaElement;
+    audio.setAttribute('src', song.songUrl + '.mp3');
+    this.service.playSong(audio);
+    document.querySelector('.songName').textContent = song.title;
+    document.querySelector('.author').textContent = song.author
+    document.querySelector('.album').textContent = song.album
   }
 
 }
